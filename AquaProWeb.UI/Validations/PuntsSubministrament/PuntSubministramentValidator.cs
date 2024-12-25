a4362631b7f79ca5682efdc5de393345f4e81d8d
jtempra <jtempra@anoiaint.com>
2024-12-25 11:17:24 +0100

    lalala

---
+++ AquaProWeb.UI/Validations/PuntsSubministrament/PuntSubministramentValidator.cs
@@ -0,0 +1,25 @@
+﻿
+using AquaProWeb.Common.Responses.Abonats.Ubicacions;
+using FluentValidation;
+
+namespace AquaProWeb.UI.Validations.PuntsSubministrament
+{
+    public class UbicacioValidator : AbstractValidator<ReadUbicacioDTO>
+    {
+        public UbicacioValidator()
+        {
+            RuleFor(c => c.Numero)
+                .NotEmpty().WithMessage("El número del punt de subministrament no pot estar en blanc");
+
+        }
+
+        // funció de validació
+        public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+        {
+            var result = await ValidateAsync(ValidationContext<ReadUbicacioDTO>.CreateWithOptions((ReadUbicacioDTO)model, x => x.IncludeProperties(propertyName)));
+            if (result.IsValid)
+                return Array.Empty<string>();
+            return result.Errors.Select(e => e.ErrorMessage);
+        };
+    }
+}
